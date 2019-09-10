97f70d10cf1909b8bd3558c6a8460c8d52bc1564
McKenna Schaller <mckenna@clickswebdesign.com>
2019-09-10 13:42:15 -0500

    working with events in directives

--- src/app/favorite.directive.ts
+++ src/app/favorite.directive.ts
@@ -1,10 +1,17 @@
-import { Directive, HostBinding, Input } from '@angular/core';
+import { Directive, HostBinding, Input, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[mwFavorite]'
 })
 export class FavoriteDirective {
   @HostBinding('class.is-favorite') isFavorite = true;
+  @HostBinding('class.is-favorite-hovering') hovering = false; // handles adding and removing css class on hover events
+  @HostListener('mouseenter') onMouseEnter() {
+      this.hovering = true;
+  } // use native events without the 'on'
+  @HostListener('mouseleave') onMouseLeave() {
+      this.hovering = false;
+  } // use native events without the 'on'
   @Input() set mwFavorite(value) {
     this.isFavorite = value;
   }
