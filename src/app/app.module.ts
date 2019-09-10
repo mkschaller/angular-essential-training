07d1cbbdad16f290b9f70e4e2c558a4690cd920a
McKenna Schaller <mckenna@clickswebdesign.com>
2019-09-10 14:00:31 -0500

    custom pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,19 @@ import { AppComponent } from './app.component';
 import { MediaItemComponent } from './media-item.component';
 import { MediaItemListComponent } from './media-item-list.component';
 import { FavoriteDirective } from './favorite.directive';
+import { CategoryListPipe } from './category-list.pipe';
 
 @NgModule({
   imports: [
     BrowserModule
   ],
+  // declarations is for components, directives, and pipes
   declarations: [
     AppComponent,
     MediaItemComponent,
     MediaItemListComponent,
-    FavoriteDirective
+    FavoriteDirective,
+    CategoryListPipe
   ],
   bootstrap: [
     AppComponent
